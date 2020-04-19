d85fe230d20f043c8f67131ece6da778101ecb60
bryanvermaeren <bryan.vermaeren@student.howest.be>
2020-04-19 18:15:11 +0200

    'flatlist'

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,12 +1,85 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, FlatList, View, Image, TouchableOpacity } from 'react-native';
 import { Header } from 'react-native-elements';
 
+import core from '../style/core';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Coca Cola',
+    content: '33 cl',
+    price: '€1,50',
+    img: require('../img/photo-of-coca-cola-bottle-2668308.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Limonade',
+    content: '33 cl',
+    price: '€1,30',
+    img: require('../img/photo-of-lemon-slices-inside-bottle-3651045.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Carlsberg',
+    content: '33 cl',
+    price: '€2,20',
+    img: require('../img/green-glass-liquor-bottle-1435598.jpg'),
+  },
+  {
+    id: '4',
+    title: 'Corona',
+    content: '33 cl',
+    price: '€2,60',
+    img: require('../img/person-holding-corona-extra-bottle-2921584.jpg'),
+  },
+  {
+    id: '5',
+    title: 'Whiskey',
+    content: '10 cl',
+    price: '€8,10',
+    img: require('../img/alcohol-alcoholic-beverage-cold-339696.jpg'),
+  },
+  {
+    id: '6',
+    title: 'Mojito',
+    content: '22 cl',
+    price: '€3,30',
+    img: require('../img/lime-cocktail-drink-with-two-straws-1187766.jpg'),
+  },
+];
+
+function Item({ title, content, price, img, selected, onSelect }) {
+  return (
+    <TouchableOpacity onPress={() => onSelect(id)} style={[core.item, { backgroundColor: selected ? '#000' : '#fff' }]}>
+      <Image source={img} style={{ width: 100, height: 100 }}></Image>
+      <View style={core.info}>
+        <Text style={core.title}>
+          {title} - {content}
+        </Text>
+        <Text style={core.price}>{price}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
 const Menu = () => {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    (id) => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
   return (
     <>
       <Header centerComponent={{ text: 'ThirstyTap', style: { color: '#fff', fontSize: 24, fontWeight: '600' } }} backgroundColor="#FF4E3A" containerStyle={{ height: 64, paddingBottom: 24 }} />
-      <Text>Menu</Text>
+      <FlatList data={DATA} renderItem={({ item }) => <Item title={item.title} content={item.content} price={item.price} img={item.img} selected={!!selected.get(item.id)} onSelect={onSelect} />} keyExtractor={(item) => item.id} extraData={selected} />
     </>
   );
 };
--- style/core.js
+++ style/core.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 import colors from './colors';
 
 export default StyleSheet.create({
   backgroundDark: {
-    backgroundColor: colors.neutral[900]
+    backgroundColor: colors.neutral[900],
   },
   backgroundLight: {
-    backgroundColor: colors.neutral[100]
-  }
+    backgroundColor: colors.neutral[100],
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 5,
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: colors.neutral[800],
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: colors.neutral[700],
+    paddingTop: 8,
+  },
 });
